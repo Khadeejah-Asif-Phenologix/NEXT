691a0861f37952e1176b01128246d2002a74f490
Khadeejah-Asif-Phenologix <khadeeja.asif@phenologix.com>
2024-12-11 14:48:02 +0500

    My Changes

--- my-app/src/app/comments/route.js
+++ my-app/src/app/comments/route.js
@@ -1,5 +1,5 @@
 import { comments } from "./data";
-import { NextRequest } from "next/server";
+// import { NextRequest } from "next/server";
 
 export async function GET(request) {
     const searchParams = request.nextUrl.searchParams;
